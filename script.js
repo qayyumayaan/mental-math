5ce3a97b2608ae87f8d109b9b1b09418487af55b
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-05-16 15:07:18 -0400

    Add script to generate a new number (temporary)

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("startButton").addEventListener("click", function() {
+    // Hide the main content and show the game content
+    document.getElementById("mainContent").classList.add("d-none");
+    document.getElementById("gameContent").classList.remove("d-none");
+
+    // Generate and display a random number
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+    document.getElementById("numberDisplay").textContent = randomNumber;
+});
+
+document.getElementById("generateButton").addEventListener("click", function() {
+    // Generate and display a new random number
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+    document.getElementById("numberDisplay").textContent = randomNumber;
+});
